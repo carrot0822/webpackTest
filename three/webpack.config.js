cc55ce766a7a0752fd234b32d625bede39cc68c4
Your Name <a384713827@qq.com>
2019-06-12 23:26:43 +0800

    html-webpack

---
+++ three/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+const config = {
+    entry:'./main.js',
+    output:{
+        filename:'bundle.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:`${Math.random()}__Zero`,
+            filename:'index.html'
+        })
+    ]
+}
+
+module.exports = config
+/*
+config说明
+entry：js文件入口
+output：{
+    filename： 输出文件的名字
+}
+*/
+/*
+HtmlWebpackPlugin说明
+title:用于生成页面的title元素
+filename:输出的HTML文件名 可以直接配置带名称？
+
+*/
